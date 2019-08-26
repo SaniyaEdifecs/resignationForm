2ce8266fecd06067e4822d01f506139359534b8c
SaniyaEdifecs <50659774+SaniyaEdifecs@users.noreply.github.com>
2019-08-26 17:30:01 +0530

    resignation form

--- src/webparts/resignationForm/components/ResignationForm.tsx
+++ src/webparts/resignationForm/components/ResignationForm.tsx
@@ -1,22 +1,33 @@
 import * as React from 'react';
 import { useState, useContext } from 'react'
 import styles from './ResignationForm.module.scss';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import Grid from '@material-ui/core/Grid';
+import {Avatar, TextareaAutosize, Button, CssBaseline, TextField, Grid, Typography, Container, Select, MenuItem, FormControl, InputLabel } from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-import Select from '@material-ui/core/Select';
-import { MenuItem, FormControl, InputLabel } from '@material-ui/core';
+
+
+// import DateFnsUtils from '@date-io/date-fns';
+// import {
+//     MuiPickersUtilsProvider,
+//   KeyboardTimePicker,
+//   KeyboardDatePicker,
+// } from '@material-ui/pickers';
+
+
 
 const ResignationForm = () => {
-  const [values, setValues] = React.useState({
-    age: '',
-    name: 'hai',
-  });
+
+  const [selectedDate, setSelectedDate] = useState<Date | null>(
+    new Date('2014-08-18T21:11:54'),
+  );
+
+  const [reason, setResignationReason] = useState('0');
+
+
+  const handleChange = (event) => {
+    console.log("evnet", event);
+    setResignationReason(event.target.value as string);
+  };
+
   return <div>
     <Container component="main" maxWidth="xs">
       <CssBaseline />
@@ -29,19 +40,19 @@ const ResignationForm = () => {
         </Typography>
         <form noValidate>
           <TextField variant="outlined" margin="normal" required fullWidth id="email" label="Employee Code" name="email" autoComplete="email" autoFocus />
-          <Grid container >
-            <Grid item>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
               <TextField variant="outlined" margin="normal" required fullWidth label="First Name" name="firstName" autoComplete="firstName" autoFocus />
             </Grid>
-            <Grid item>
-              <TextField variant="outlined" margin="normal" required fullWidth id="email" label="Last Name" name="lastName" autoComplete="lastName" autoFocus />
+            <Grid item xs={12} sm={6}>
+              <TextField variant="outlined" margin="normal" required fullWidth label="Last Name" name="lastName" autoComplete="lastName" autoFocus />
             </Grid>
           </Grid>
-          <TextField variant="outlined" margin="normal" required fullWidth id="email" label="Work Email" name="email" autoComplete="email" autoFocus />
-          <TextField variant="outlined" margin="normal" required fullWidth id="email" label="Personal Email" name="email" autoComplete="email" autoFocus />
-          <FormControl >
-            <InputLabel htmlFor="age-simple">Reason for Resignation</InputLabel>
-            <Select inputProps={{ name: 'age', id: 'age-simple', }} value="Personal" >
+          <TextField variant="outlined" margin="normal" required fullWidth label="Work Email" name="workEmail" autoComplete="workEmail" autoFocus />
+          <TextField variant="outlined" margin="normal" required fullWidth  label="Personal Email" name="personalEmail" autoComplete="personalEmail" autoFocus />
+          <FormControl className="MuiFormControl-root MuiTextField-root MuiFormControl-marginNormal MuiFormControl-fullWidth">
+            <InputLabel htmlFor="reason">Reason for Resignation</InputLabel>
+            <Select value={reason}  onChange={handleChange} >
               <MenuItem value={10}>Personal</MenuItem>
               <MenuItem value={20}>Health</MenuItem>
               <MenuItem value={30}>Better Oppertunity</MenuItem>
@@ -51,9 +62,40 @@ const ResignationForm = () => {
               <MenuItem value={70}>Other</MenuItem>
             </Select>
           </FormControl>
-          <TextField variant="outlined" margin="normal" required fullWidth id="email" label="Specify(if other is selected)" name="email" autoFocus />
+          
+          <TextField variant="outlined" margin="normal" required fullWidth label="Department" name="department" autoFocus />
+          <TextField variant="outlined" margin="normal" required fullWidth label="Title" name="title" autoFocus />
+          
+          {/* <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <Grid container justify="space-around">
+        <KeyboardDatePicker
+          disableToolbar
+          variant="inline"
+          format="MM/dd/yyyy"
+          margin="normal"
+          id="date-picker-inline"
+          label="Date picker inline"
+          value={selectedDate}
+          onChange={handleDateChange}
+          KeyboardButtonProps={{
+            'aria-label': 'change date',
+          }}
+        />
+        </Grid>
+        </MuiPickersUtilsProvider> */}
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+            <TextField variant="outlined" margin="normal" required fullWidth label="First Name" name="managerFirstName" autoFocus />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField variant="outlined" margin="normal" required fullWidth label="Last Name" name="managerLastName" autoComplete="lastName" autoFocus />
+            </Grid>
+          </Grid>
+          <TextField variant="outlined" margin="normal" required fullWidth label="Manager Email" name="managerEmail" autoFocus />
+          <TextField id="outlined-textarea" className="MuiFormControl-root MuiTextField-root MuiFormControl-marginNormal MuiFormControl-fullWidth" label="Resignation Summary" placeholder="Resignation Summary" multiline margin="normal" variant="outlined" />
+    {/* <TextareaAutosize aria-label="minimum height" rows={3} placeholder="Resignation Summary" />;     */}
           <Button type="submit" fullWidth variant="contained" color="primary">
-            Sign In
+            Submit
           </Button>
         </form>
       </div>
